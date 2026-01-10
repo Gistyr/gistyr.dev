4b24aa21f633468219746419473d51b87ece9bbf
lozlof <contact@gistyr.dev>
2026-01-10 18:56:08 -0500

    Text sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,8 +22,8 @@ const LINES = [
     contentClassName: "space-y-2",
     content: (
       <>
-        <p className="text-xl">Gistyr is developing an online, multiplayer, skill-based board game.</p>
-        <p className="text-xl">The open beta will be released on the web in November 2026.</p>
+        <p className="text-lg lg:text-xl">Gistyr is developing an online, multiplayer, skill-based board game.</p>
+        <p className="text-lg lg:text-xl">The open beta will be released on the web in November 2026.</p>
       </>
     ),
   },
@@ -33,8 +33,8 @@ const LINES = [
     contentClassName: "space-y-2",
     content: (
       <>
-        <p>Gistyr is developing an online, multiplayer, skill-based board game.</p>
-        <p>The open beta will be released on the web in November 2026.</p>
+        <p className="text-lg lg:text-xl">Gistyr is developing an online, multiplayer, skill-based board game.</p>
+        <p className="text-lg lg:text-xl">The open beta will be released on the web in November 2026.</p>
       </>
     ),
   },
@@ -44,16 +44,16 @@ const LINES = [
     contentClassName: "space-y-2",
     content: (
       <>
-        <p>Gistyr's primary product will be powered by proprietary, multiplayer server infrastructure.</p>
-        <p>The open beta will be released on the web in November 2026.</p>
+        <p className="text-lg lg:text-xl">Gistyr's primary product will be powered by proprietary, multiplayer server infrastructure.</p>
+        <p className="text-lg lg:text-xl">The open beta will be released on the web in November 2026.</p>
         <div className="h-6" />
-        <p>One of Gistyr's open source projects, <span className="font-medium">GOBSG</span>, is a secure OpenID Connect Backend-for-Frontend (BFF) gateway.</p>
-        <p>It provides cookie-based session management for web clients and is written in Rust.</p>
+        <p className="text-lg lg:text-xl">One of Gistyr's open source projects, <span className="font-medium">GOBSG</span>, is a secure OpenID Connect Backend-for-Frontend (BFF) gateway.</p>
+        <p className="text-lg lg:text-xl">It provides cookie-based session management for web clients and is written in Rust.</p>
         <a
           href="https://gistyr.dev/gobsg"
           className="
             inline-block
-            text-sm
+            text-md lg:text-lg
             text-blue-400
             hover:text-blue-300
             underline
